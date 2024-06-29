4e11d0c9270d89fbd3f8ce2cc56c1ef60d962dfe
uU-Uuu <ustja.54@gmail.com>
2024-06-29 18:56:03 +0300

    f0 calculation min max median mode mean

--- client/js/index.js
+++ client/js/index.js
@@ -14,6 +14,83 @@ function getFileIdFromStorage() {
 }
 
 
+function freqToNote(freq) {
+    if (freq === 0 || freq === 'NaN') return NaN;
+    const note = Tone.Frequency(freq, 'hz').toNote();
+    return note;
+}
+
+
+function calculateF0(data) {
+    
+    const f0Arr = data['f0'];
+
+    const f0Checkboxes = Array.from(document.querySelectorAll('.f0-check-option:checked')).map(checkb => checkb.value);
+    let f0Values = {}
+
+    if (f0Checkboxes.length === 0) {
+        const mean = f0Arr.reduce((sum, curr) => sum + curr, 0 ) / f0Arr.length;
+        f0Values['mean'] = [mean, freqToNote(mean)];
+
+    } else {
+
+        if (!f0Checkboxes.includes('mean')) {
+            f0Values = {};
+        };
+
+        if (f0Checkboxes.includes('median')) {
+            const sorted = f0Arr.slice().sort((a, b) => a - b);
+            const mid = Math.floor(sorted.length / 2);
+            const median = sorted.length % 2 === 0 ? (sorted[mid - 1] + sorted[mid + 1]) / 2 : sorted[mid];
+            f0Values['median'] = [median, freqToNote(median)];
+        };
+
+        if (f0Checkboxes.includes('mode')) {
+            const sorted = f0Arr.slice().sort((a, b) => a - b)
+                                        .filter(num => num !== 0)
+                                        .map(num => Math.round(num, 0))
+                                        .map(freq => freqToNote(freq));
+            let maxStreak = 0;
+            let currStreak = 1;
+            let currEl = sorted[0];
+            let mode = 1;
+
+            for (let i = 0; i < sorted.length; i++) {
+                if (sorted[i] === sorted[i + 1] && sorted[i] !== 0) {
+                    currStreak++;
+                } else {
+                    if (currStreak > maxStreak) {
+                        maxStreak = currStreak;
+                        mode = currEl
+                    }
+                }
+                currStreak = 1;
+                currEl = sorted[i];
+            };
+
+            if (currStreak > maxStreak) {
+                maxStreak = currStreak;
+                mode = currEl
+            }
+            const modeFin = maxStreak !== 1 ? mode : 'NaN';
+            f0Values['mode'] = [modeFin, freqToNote(modeFin)];
+        };
+
+        if (f0Checkboxes.includes('max')) {
+            const max = f0Arr.reduce((max, curr) => curr > max ? curr : max);
+            f0Values['max'] = [max, freqToNote(max)];
+        };
+
+        if (f0Checkboxes.includes('min')) {
+            const min = f0Arr.reduce((min, curr) => curr < min ? curr : min);
+            f0Values['min'] = [min, freqToNote(min)];
+        };
+    }
+    return f0Values;
+
+}
+
+
 uploadBtn.addEventListener( 'click', async () => {
 
     const uploadedFile = fileInput.files[0];
@@ -67,8 +144,10 @@ fetch(`${BASE_URL}/f0?id=${storedID}`)
         return response.json();
     })
     .then(data => {
-        console.log('Data received ->', data);    
+        const a = calculateF0(data);
+        console.log(a)
     })
+
     .catch(err => {
         console.error('Fetch error: ', err)
     })
