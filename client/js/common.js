58dd1460110f1b53b05f740abc0f23cced71a898
uU-Uuu <ustja.54@gmail.com>
2024-07-01 09:35:08 +0300

    display and play f0 values

--- client/js/common.js
+++ client/js/common.js
@@ -1,10 +1,15 @@
-export function getFileIdFromStorage() { 
-    return sessionStorage.getItem('uploadID');
+export function getFileIdFromStorage() {
+  return sessionStorage.getItem("uploadID");
 }
 
-
 export function freqToNote(freq) {
-    if (freq === 0 || freq === 'NaN') return NaN;
-    const note = Tone.Frequency(freq, 'hz').toNote();
-    return note;
+  if (freq === 0 || freq === "-") return "-";
+  const note = Tone.Frequency(freq, "hz").toNote();
+  return note;
+}
+
+export function playFreq(freq, volume, duration) {
+  const osc = new Tone.Oscillator(freq, "triangle").toDestination();
+  osc.volume.value = volume;
+  osc.start().stop(`+${duration}`);
 }
--- client/js/f0.js
+++ client/js/f0.js
@@ -1,159 +1,213 @@
-import { getFileIdFromStorage, freqToNote } from './common.js';
-import { BASE_URL } from './env.js';
+import { getFileIdFromStorage, freqToNote, playFreq } from "./common.js";
+import { BASE_URL } from "./env.js";
 
-
-const f0Btn = document.querySelector('.f0__f0-btn');
-const spectrBtn = document.querySelector('.f0__spectro-btn');
-const spectrF0Btn = document.querySelector('.f0__f0-spectro-btn');
-const f0Lbl = document.querySelector('.f0__label');
-
+const f0Btn = document.querySelector(".f0__f0-btn");
+const spectrBtn = document.querySelector(".f0__spectro-btn");
+const spectrF0Btn = document.querySelector(".f0__f0-spectro-btn");
+const f0Lbl = document.querySelector(".f0__label");
+const allCheck = document.querySelector(".f0__check-all");
 
 function calculateF0(data) {
-    
-    const f0Arr = data['f0'];
+  const f0Arr = data["f0"].filter((val) => val !== 0);
 
-    const f0Checkboxes = Array.from(document.querySelectorAll('.f0__check-option:checked')).map(checkb => checkb.value);
-    let f0Values = {}
+  let f0Checkboxes = Array.from(
+    document.querySelectorAll(".f0__check-option:checked")
+  ).map((checkb) => checkb.value);
+  let f0Values = {};
 
-    if (f0Checkboxes.length === 0) {
-        const mean = f0Arr.reduce((sum, curr) => sum + curr, 0 ) / f0Arr.length;
-        f0Values['mean'] = [mean, freqToNote(mean)];
+  if (f0Checkboxes.length === 0) {
+    allCheck.checked = true;
+    f0Checkboxes = checkAll().map((checkb) => checkb.value);
+  }
+  if (f0Checkboxes.includes("mean")) {
+    const mean = Math.round(
+      f0Arr.reduce((sum, curr) => sum + curr, 0) / f0Arr.length,
+      0
+    );
+    f0Values["mean"] = [mean, freqToNote(mean)];
+  }
 
-    } else {
+  if (f0Checkboxes.includes("median")) {
+    const sorted = f0Arr.slice().sort((a, b) => a - b);
+    const mid = Math.floor(sorted.length / 2);
+    const median =
+      sorted.length % 2 === 0
+        ? (sorted[mid - 1] + sorted[mid + 1]) / 2
+        : sorted[mid];
+    f0Values["median"] = [Math.round(median, 0), freqToNote(median)];
+  }
 
-        if (f0Checkboxes.includes('mean')) {
-            const mean = f0Arr.reduce((sum, curr) => sum + curr, 0 ) / f0Arr.length;
-            f0Values['mean'] = [mean, freqToNote(mean)];
-        };
+  if (f0Checkboxes.includes("mode")) {
+    const sorted = f0Arr.slice().sort((a, b) => a - b);
+    let maxStreak = 0;
+    let currStreak = 1;
+    let currEl = sorted[0];
+    let mode = sorted[0];
 
-        if (f0Checkboxes.includes('median')) {
-            const sorted = f0Arr.slice().sort((a, b) => a - b);
-            const mid = Math.floor(sorted.length / 2);
-            const median = sorted.length % 2 === 0 ? (sorted[mid - 1] + sorted[mid + 1]) / 2 : sorted[mid];
-            f0Values['median'] = [median, freqToNote(median)];
-        };
-
-        if (f0Checkboxes.includes('mode')) {
-            const sorted = f0Arr.slice().sort((a, b) => a - b)
-                                        .filter(num => num !== 0)
-                                        .map(num => Math.round(num, 0))
-                                        .map(freq => freqToNote(freq));
-            let maxStreak = 0;
-            let currStreak = 1;
-            let currEl = sorted[0];
-            let mode = 1;
-
-            for (let i = 0; i < sorted.length; i++) {
-                if (sorted[i] === sorted[i + 1] && sorted[i] !== 0) {
-                    currStreak++;
-                } else {
-                    if (currStreak > maxStreak) {
-                        maxStreak = currStreak;
-                        mode = currEl
-                    }
-                }
-                currStreak = 1;
-                currEl = sorted[i];
-            };
-
-            if (currStreak > maxStreak) {
-                maxStreak = currStreak;
-                mode = currEl
-            }
-            const modeFin = maxStreak !== 1 ? mode : 'NaN';
-            f0Values['mode'] = [modeFin, freqToNote(modeFin)];
-        };
-
-        if (f0Checkboxes.includes('max')) {
-            const max = f0Arr.reduce((max, curr) => curr > max ? curr : max);
-            f0Values['max'] = [max, freqToNote(max)];
-        };
-
-        if (f0Checkboxes.includes('min')) {
-            const min = f0Arr.reduce((min, curr) => curr < min ? curr : min);
-            f0Values['min'] = [min, freqToNote(min)];
-        };
+    for (let i = 0; i < sorted.length; i++) {
+      if (sorted[i] === sorted[i + 1]) {
+        currStreak++;
+      } else {
+        if (currStreak > maxStreak) {
+          maxStreak = currStreak;
+          mode = currEl;
+        }
+        currStreak = 1;
+      }
+      currEl = sorted[i];
     }
-    return f0Values;
+
+    if (currStreak > maxStreak) {
+      maxStreak = currStreak;
+      mode = currEl;
+    }
+    const modeFin = maxStreak !== 1 ? mode : "-";
+    f0Values["mode"] = [Math.round(modeFin, 0), freqToNote(modeFin)];
+  }
+
+  if (f0Checkboxes.includes("max")) {
+    const max = Math.round(
+      f0Arr.reduce((max, curr) => (curr > max ? curr : max)),
+      0
+    );
+    f0Values["max"] = [max, freqToNote(max)];
+  }
+
+  if (f0Checkboxes.includes("min")) {
+    const min = Math.round(
+      f0Arr.reduce((min, curr) => (curr < min ? curr : min)),
+      0
+    );
+    f0Values["min"] = [min, freqToNote(min)];
+  }
+
+  return f0Values;
 }
 
-
 function retrieveSpectrogram(url, div) {
+  const storedID = getFileIdFromStorage();
+  if (!storedID) {
+    document.querySelector(".f0-label").innerHTML = "No file selected.";
+    return;
+  }
 
-    const storedID = getFileIdFromStorage();
-    if (!storedID) {
-        document.querySelector('.f0-label').innerHTML = 'No file selected.';
-        return;
-    }
-
-    fetch(`${BASE_URL}/${url}?id=${storedID}`)
-
-    .then(response => {
-        if(!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.blob();
+  fetch(`${BASE_URL}/${url}?id=${storedID}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.blob();
     })
 
-    .then(blob => {
-        const url = URL.createObjectURL(blob);
-        const spectroCont = document.querySelector(div);
-        spectroCont.innerHTML = `
+    .then((blob) => {
+      const url = URL.createObjectURL(blob);
+      const spectroCont = document.querySelector(div);
+      spectroCont.innerHTML = `
         <img src="${url}" alt="Spectrogram">
         `;
     })
 
-    .catch(err => {
-        console.error('Fetch error: ', err);
-    })
+    .catch((err) => {
+      console.error("Fetch error: ", err);
+    });
 }
 
+function checkAll() {
+  const f0Checkbx = document.querySelectorAll(".f0__check-option");
+  if (allCheck.checked) {
+    f0Checkbx.forEach((bx) => {
+      bx.disabled = true;
+      bx.checked = true;
+    });
+  } else {
+    f0Checkbx.forEach((bx) => {
+      bx.disabled = false;
+      bx.checked = false;
+    });
+  }
+  return Array.from(f0Checkbx);
+}
 
-const allCheck = document.querySelector('.f0__check-all');
-allCheck.addEventListener('click', () => {
-    const f0Checkbx = document.querySelectorAll('.f0__check-option'); 
-    f0Checkbx.forEach( bx => {
-        bx.disabled = true;
-        bx.checked = true;
+allCheck.addEventListener("click", () => {
+  checkAll();
+});
+
+f0Btn.addEventListener("click", async () => {
+  const storedID = getFileIdFromStorage();
+
+  if (!storedID) {
+    document.querySelector(".f0__label").innerHTML = "No file selected.";
+    return;
+  }
+
+  fetch(`${BASE_URL}/f0?id=${storedID}`)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
     })
-})
+    .then((data) => {
+      if ("message" in data) {
+        f0Lbl.innerHTML = "Session has expired. Please upload the file again.";
+      } else {
+        try {
+          const f0Obj = calculateF0(data);
+          let tableRowsDomString = "";
 
+          for (const [key, value] of Object.entries(f0Obj)) {
+            let btnDomString = "";
 
-f0Btn.addEventListener('click', async () => {
-    const storedID = getFileIdFromStorage();
+            console.log(key, value);
 
-    if (!storedID) {
-        document.querySelector('.f0__label').innerHTML = 'No file selected.';
-        return;
+            if (value[1] !== "-") {
+              btnDomString = `
+                <button class="f0__tablecont-table-playbtn btnExists" value=${value[0]}>
+                    <img class="f0__tablecont-table-playbtn-icon" src="img/play-icon.svg" />
+                </button>
+              `;
+            }
+            tableRowsDomString += `
+                        <tr>
+                            <th scope="col">${key}</th>
+                            <td>${value[0]}</td>
+                            <td>${value[1]}</td>
+                            <td>${btnDomString}</td>
+                        </tr>
+                        `;
+          }
+          document.querySelector(".f0__tablecont-table tbody").innerHTML =
+            tableRowsDomString;
+          document.querySelector(".f0__tablecont-table").style.visibility =
+            "visible";
+        } catch (err) {
+          console.log(err);
+        }
+      }
+    })
+
+    .catch((err) => {
+      f0Lbl.innerHTML = "Please upload a file.";
+      console.error("Fetch error: ", err);
+    });
+});
+
+document
+  .querySelector(".f0__tablecont-table")
+  .addEventListener("click", (e) => {
+    let targ = e.target;
+    if (e.target && e.target.tagName !== "BUTTON") {
+      targ = e.target.parentElement;
     }
-    
-    fetch(`${BASE_URL}/f0?id=${storedID}`)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
-            if ('message' in data) {
-                f0Lbl.innerHTML = 'Session has expired. Please upload the file again.';
-            } else {
-                const a = calculateF0(data);
-                console.log(a);
-            }
-        })
+    if (targ && targ.classList.contains("btnExists")) {
+      playFreq(targ.value, -15, 2);
+    }
+  });
 
-        .catch(err => {
-            f0Lbl.innerHTML = 'Please upload a file.'
-            console.error('Fetch error: ', err)
-        })
-})
+spectrBtn.addEventListener("click", async () => {
+  retrieveSpectrogram("spectrogram", ".f0__spectrogram-cont");
+});
 
-
-spectrBtn.addEventListener('click', async () => {
-    retrieveSpectrogram('spectrogram', '.f0__spectrogram-cont');
-})
-
-spectrF0Btn.addEventListener('click', async () => {
-    retrieveSpectrogram('f0/spectrogram', '.f0__f0-spectrogram-cont');
+spectrF0Btn.addEventListener("click", async () => {
+  retrieveSpectrogram("f0/spectrogram", ".f0__f0-spectrogram-cont");
 });
